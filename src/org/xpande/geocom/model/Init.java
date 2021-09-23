1561d7374104cb835fa7ba4289d03aed50fd670a
gabriel <gabrielvila13@gmail.com>
2021-09-23 09:46:34 -0300

    Initial commit

---
+++ src/org/xpande/geocom/model/Init.java
@@ -0,0 +1,9 @@
+package org.xpande.geocom.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 9/23/21.
+ */
+public class Init {
+
+}
